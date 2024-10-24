b6127c029f06daf2fb1ebe96ad514a7e2b5bb0c4
RRK <130690175+rock-star-2003@users.noreply.github.com>
2024-10-25 00:41:42 +0530

    Update urls.py

--- System/urls.py
+++ System/urls.py
@@ -22,3 +22,6 @@ urlpatterns = [
     path('',include('home.urls')),
     path('',include('django.contrib.auth.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
