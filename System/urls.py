7c79a3286afd92ae0435db30778827acbe71d4c2
RRK <130690175+rock-star-2003@users.noreply.github.com>
2024-10-25 00:17:16 +0530

    Update urls.py

--- System/urls.py
+++ System/urls.py
@@ -19,6 +19,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('student-management-y77c.vercel.app',include('home.urls')),
+    path('',include('home.urls')),
     path('',include('django.contrib.auth.urls')),
 ]
